dea866c16e7a755ca93752bd19b706208d62643b
SamehElMalah <sameh.mahmoud@algorithmz.net>
2020-12-04 13:14:06 +0200

    add cities view controller

---
+++ WeatherApp/Core/LocalServices/Cities/CitiesService.swift
@@ -0,0 +1,34 @@
+//
+//  CitiesService.swift
+//  WeatherApp
+//
+//  Created by sameh on 12/4/20.
+//
+
+import Foundation
+
+typealias Cities = [String]
+
+class CitiesService {
+    func loadAllCities() -> Cities? {
+        
+        var contentData: Data?
+        if let path = Bundle.main.path(forResource: "cities", ofType: ".json") {
+            contentData = try? Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+        }
+
+        guard let data = contentData else {
+            return nil
+        }
+
+        // Read data from .json
+        do {
+            let jsonModel = try JSONDecoder().decode(Cities.self, from: data)
+            return jsonModel
+        } catch {
+            print(error)
+        }
+        
+        return nil
+    }
+}
---
+++ WeatherApp/Modules/CitiesWeatherForecast/Protocols/CititesWeatherForecast.swift
@@ -0,0 +1,19 @@
+//
+//  CititesWeatherForecast.swift
+//  WeatherApp
+//
+//  Created by sameh on 12/4/20.
+//
+
+import Foundation
+
+// MARK: View -
+protocol CitiesWeatherForecastViewProtocol: class {
+
+}
+
+// MARK: Presenter -
+protocol CitiesWeatherForecastPresenterProtocol: class {
+    var view: CitiesWeatherForecastViewProtocol? { get set }
+    func viewDidLoad()
+}
